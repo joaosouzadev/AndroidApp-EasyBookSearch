6234f2f1dd182de84462de01e178b1617953cb97
João Victor Souza <jvms3d@gmail.com>
2018-04-24 12:05:28 -0300

    Criados os layouts .XML para tela inicial e lista de livros.

--- app/src/main/java/com/example/android/easybooksearch/MainActivity.java
+++ app/src/main/java/com/example/android/easybooksearch/MainActivity.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public static final String LOG_TAG = MainActivity.class.getName();
+
+    private static final String BOOK_REQUEST_URL =
+            "https://www.googleapis.com/books/v1/volumes?q=android&maxResults=3";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
